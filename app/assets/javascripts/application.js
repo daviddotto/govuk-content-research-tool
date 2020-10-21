fb42d4f29ff231fb346e231f36c3b44a775d62ff
David Todd <david.todd@cqc.org.uk>
2020-10-21 16:27:06 +0100

    updates to selection logic

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,76 +2,95 @@
 
 // Warn about using the kit in production
 if (window.console && window.console.info) {
-  window.console.info('GOV.UK Prototype Kit - do not use for production')
+	window.console.info('GOV.UK Prototype Kit - do not use for production')
 }
 
 $(document).ready(function () {
-  window.GOVUKFrontend.initAll()
-  contentFeedback = {}
+	window.GOVUKFrontend.initAll()
+	contentFeedback = {}
 })
 
 var CFObject
 
 function highlight(text, isPositive) {
-  var inputText = document.getElementById("contentToTest");
-  var innerHTML = inputText.innerHTML;
-  inputText.innerHTML = innerHTML.replace(new RegExp(text, "ig"), function(phrase) {
-    return '<mark class="content-highlight ' + (isPositive ? 'content-highlight-good' : 'content-highlight-bad') + '">' + phrase + '</mark>'
-  })
+	var inputText = document.getElementById('contentToTest')
+	var innerHTML = inputText.innerHTML
+	inputText.innerHTML = innerHTML.replace(new RegExp(text, 'ig'), function (
+		phrase
+	) {
+		return (
+			'<mark class="content-highlight ' +
+			(isPositive ? 'content-highlight-good' : 'content-highlight-bad') +
+			'">' +
+			phrase +
+			'</mark>'
+		)
+	})
 }
 
 function performFormatting() {
-  $('#contentToTest mark.content-highlight').contents().unwrap()
-  for (phrase of CFObject.feedback.good) {
-    highlight(phrase, true)
-  }
-  for (phrase of CFObject.feedback.bad) {
-    highlight(phrase, false)
-  }
+	$('#contentToTest mark.content-highlight').contents().unwrap()
+	CFObject.feedback.good.sort(function (a, b) {
+		return b.length - a.length
+  })
+  CFObject.feedback.bad.sort(function (a, b) {
+		return b.length - a.length
+	})
+	for (phrase of CFObject.feedback.good) {
+		highlight(phrase, true)
+	}
+	for (phrase of CFObject.feedback.bad) {
+		highlight(phrase, false)
+	}
 }
 
 var ContentResearch = {
-  feedback : {
-    good:[],
-    bad: []
-  },
-  vote: function(selection, isPositive) {
-    var asLines = selection.split('\n');
-    asLines = asLines.filter(line => { return line.trim().length != 0 })
-    for (line of asLines) {
-      if (isPositive) {
-        this.feedback.good.push(line.trim())
-      } else {
-        this.feedback.bad.push(line.trim())
-      }
-    }
-    performFormatting()
-    console.log(this.feedback)
-  },
-  voteUp: function() {
-    var selection = window.getSelection().toString()
-    if (selection) {
-      this.vote(selection.trim(), true)
-    }
-  },
-  voteDown: function() {
-    var selection = window.getSelection().toString()
-    if (selection) {
-      this.vote(selection, false)
-    }
-  }
+	feedback: {
+		good: [],
+		bad: [],
+	},
+	vote: function (selection, isPositive) {
+		var asLines = selection.split('\n')
+		asLines = asLines.filter((line) => {
+			return line.trim().length > 2
+		})
+		for (line of asLines) {
+			if (isPositive) {
+				this.feedback.good.push(line.trim())
+			} else {
+				this.feedback.bad.push(line.trim())
+			}
+		}
+		performFormatting()
+		console.log(this.feedback)
+	},
+	voteUp: function () {
+		var selection = window.getSelection().toString()
+		if (selection) {
+			this.vote(selection.trim(), true)
+		}
+	},
+	voteDown: function () {
+		var selection = window.getSelection().toString()
+		if (selection) {
+			this.vote(selection, false)
+		}
+	},
 }
 
 CFObject = ContentResearch
 
 function removePhrase(phrase) {
-  CFObject.feedback.good = CFObject.feedback.good.filter(instance => instance !== phrase)
-  CFObject.feedback.bad = CFObject.feedback.bad.filter(instance => instance !== phrase)
+	CFObject.feedback.good = CFObject.feedback.good.filter(
+		(instance) => instance !== phrase
+	)
+	CFObject.feedback.bad = CFObject.feedback.bad.filter(
+		(instance) => instance !== phrase
+	)
 }
 
-$(document).on('click', 'mark.content-highlight', function() {
-  var phrase = $(this).text()
-  removePhrase(phrase)
-  performFormatting()
+$(document).on('click', 'mark.content-highlight', function () {
+	var phrase = $(this).text()
+	removePhrase(phrase)
+	performFormatting()
 })
-
